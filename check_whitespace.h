c5e76a513469650093066b3c86852056073ccb73
TyBeas <beasl110@morris.umn.edu>
2025-02-11 15:47:57 -0600

    Passing tests

--- check_whitespace.h
+++ check_whitespace.h
@@ -1,11 +1,6 @@
 #ifndef CHECK_WHITESPACE_H_GUARD
 #define CHECK_WHITESPACE_H_GUARD
 
-#include "check_whitespace.h"
-
-#endif
-#define CHECK_WHITESPACE_H_GUARD
-
 char* strip(char const *str);
 int is_clean(char const *str);
 
